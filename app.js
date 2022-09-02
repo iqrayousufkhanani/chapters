b92063f11160c2d1a2fffe84d47e138484cd6ee1
iqra <iqrayousufkhanani@gmail.com>
2022-09-03 00:10:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,190 @@
+// alert ("Error!Please enter a valid password.");
+// alert("Welcome to JS Land...Happy Coding!");
+// alert("Welcome to JS Land... \n Happy Coding!")
+// alert("welcome to JS land...");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs. ")
+// alert("Hello... I can run JS through my web browser's console")
+
+// var myname ="Iqra Yousuf";
+// const message = "Hello World"
+// alert(message)
+
+// var Name="Jhone Doe";
+// alert(Name);
+// var age="15 years old";
+// alert(age);
+// var msg="Certified Mobile Application Development";
+// alert(msg);
+
+// var a = "pizza";
+// var b = a.slice(0, 5).toUpperCase();
+// var c = b.slice(0, 4);
+// var d = b.slice(0, 3);
+// var e = b.slice(0, 2);
+// var f = b.slice(0, 1);
+// alert( b + "\n" + c + "\n" + d + "\n"+ e + "\n"+ f );
+
+// var email = "example@example.com";
+// alert("my email address " + email);
+
+// var book = "A  smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// var end = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(end);
+
+// var age = "16"
+// alert("I am "+ age + " years old");
+
+// let vistingTime = 14
+// alert('you have visited this site ' + vistingTime + ' times');
+
+// var birthyear ="2006";
+// document.write(`My birth year is ${birthyear} <br> Data type  of my declared variable is number`);
+
+// var visitor = "John Doe";
+// var Product = "5 T-Shirt ";
+// var Quantity = "(s) on XYZ Clothing store ";
+// document.write(visitor +" ordered "+ Product + Quantity)
+
+// var firstname = 'iqra';
+// var secondname = "Yousuf"
+// var surname = "Khanani"
+// var bio = firstname + secondname + surname;
+// console.log(bio);
+
+// (legal)
+// var ABC = "";
+// var $123 = "";
+// var ab$c = "";
+// var _abc = "";
+// var abc_ = "";
+// (illegal)
+// var 123 = "";
+// var 12$3 = "";
+// var 12_ = "";
+// var _123 = "";
+// var 1_23 = "";
+
+// var a = "$";
+// var b ="_";
+// var c ="_1st";
+// var d ="$my";
+// var sensitive = "sensitive"
+// var keywords = "keywords"
+// var heading = "Rules for naming JS variable"
+// document.write(`${heading} <br> <br>
+//  Variable names can only contain number ,  ${a} and ${b}  For example ${a}  my  ${c} <br>
+// Variable must begin with a letter  ${a} or ${b}  For example ${a}  namme  ${b} or name <br>
+// Variable names are case ${sensitive} <br>
+// Variable names should not be JS ${sensitive}` );
+
+// var a = 3;
+// var b = 5;
+// document.write( a+b );
+
+// var a = 3;
+// var b = 5;
+// document.write( a*b);
+
+// var a = 3;
+// var b = 5;
+// document.write( a/b);
+
+// var int = 5;
+// var inc = 1+int;
+// var adt= 7+inc;
+// var dec = adt-1;
+// var div = dec%3;
+// document.write(`Value after variable declaration is undefined <br>
+// Initial value : ${int} <br>
+// Value after increament is : ${inc} <br>
+// Value after addition is : ${adt} <br>
+// Value after decrement is : ${dec} <br>
+// The reminder is ${div}`);
+
+// var cost = 600;
+// var ticket = 5;
+// var buy = cost*ticket;
+// document.write(`Total cost to buy ${ticket} tickets to a movie is ${buy}PKR`)
+
+// var table = 4
+// let body = document.getElementById('body')
+// console.log('body', body)
+// for (i = 1; i <= 10; i++) {
+//   body.innerHTML += `<ul> ${table} * ${i} = ${table * i} </ul>`
+// }
+
+// var C = 25;
+// var F = 70;
+// var oC = (F-32)*5/9;
+// var oF = (C*9/5)+32;
+// document.write(`${C}C is ${oF}F <br>
+// ${F}F is ${oC}C`)
+
+// var item1 = 650
+// var item2 = 100
+// var order1 = 3
+// var order2 = 7
+// var ship = 100
+// document.write(`Price of item 1 is ${item1} <br>
+// Quantity of item 1 is ${order1} <br>
+// Price of item 2 is ${item2} <br>
+// Quantity of item 2 is ${order2} <br> <br>`)
+// var total = (item1*order1)+(item2*order2)+ship
+// document.write(`Total cost of your order is ${total}`)
+
+// var mark = 980;
+// var obtained = 804;
+// var percentage = (obtained/mark*100)
+// document.write(`Total marks: ${mark} <br>
+// Marks obtained: ${obtained} <br>
+// percentage ${percentage}%`)
+
+// var US = 
+// var   US
+
+// var add = 5
+// var multiply = 10
+// var divide = 2
+// var sum = (add * multiply )/ divide
+// document.write(`Perform all calculation s in a single expression = ${sum}`)
+
+// var year = 2022
+// var birth = 2006
+// var birthyear = 2022-2006
+// document.write(`Current year: ${year}<br>
+// Birth year: ${birth}<br>
+// Your Age: ${birthyear}`)
+
+// life time supply
+
+// var a = 2
+// var b = 1
+// var result =--a - --b + ++b + b--
+// --a - --b + ++b + b--
+// var mmm =  1 - 0 + 1 + 1; 
+// document.write(`a is ${2}<br>
+// b is ${1}<br>
+// result is ${result}`)
+
+// var username = prompt("Enter username");
+// alert(`Hello ${username}`);
+
+// var table =prompt("Please enter any number");
+// for(var k=1; k<=10; k++){
+//     document.write (table  + " x " + k + " = " + (k*table) + "<br>")
+// }
+
+// var English = 100
+// var Math = 100
+// var Urdu = 100
+// var total =(English + Math + Urdu)
+// var a =prompt("Enter a English marks" )
+// var b =prompt("Enter a Math marks" )
+// var c =prompt("Enter a Urdu marks" )
+// var mark = `${a+ b +c}`;
+// document.write(mark)
+
+
+
